78976f9bbb26f37fabeb55fb9212d8aacdd5e3b0
Gnarkill33 <gnarkill33@gmail.com>
2024-10-28 00:23:50 +0300

    init: start js-numbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,12 +18,12 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
- * Returns a circumference of circle given by radius.
+ * Returns a circumference of circle given by radius. СДЕЛАТЬ
  *
  * @param {number} radius
  * @return {number}
@@ -33,12 +33,12 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * 3.1415926535 * radius;
 }
 
 /**
- * Returns an average of two given numbers.
+ * Returns an average of two given numbers. СДЕЛАТЬ
  *
  * @param {number} value1
  * @param {number} value2
@@ -49,8 +49,8 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return (value1 + value2) / 2;
 }
 
 /**
@@ -68,12 +68,14 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const sum1 = (x2 - x1) ** 2;
+  const sum2 = (y2 - y1) ** 2;
+  return Math.sqrt(sum1 + sum2);
 }
 
 /**
- * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
+ * Returns a root of linear equation a*x + b = 0 given by coefficients a and b. СДЕЛАТЬ
  *
  * @param {number} a
  * @param {number} b
@@ -91,7 +93,7 @@ function getLinearEquationRoot(/* a, b */) {
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
  * coordinates in Cartesian plane.
- * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations
+ * See details https://en.wikipedia.org/wiki/Euclidean_vector#Representations СДЕЛАТЬ
  *
  * @param {number} x1
  * @param {number} y1
@@ -122,8 +124,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -137,12 +139,12 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
- * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
+ * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c. СДЕЛАТЬ
  *
  * @param {number} a
  * @param {number} b
@@ -159,7 +161,7 @@ function getParallelepipedDiagonal(/* a, b, c */) {
 }
 
 /**
- * Returns the number rounded to specified power of 10.
+ * Returns the number rounded to specified power of 10. СДЕЛАТЬ
  *
  * @param {number} num
  * @param {number} pow
@@ -180,7 +182,7 @@ function roundToPowerOfTen(/* num, pow */) {
 }
 
 /**
- * Returns true is the number is prime; otherwise false.
+ * Returns true is the number is prime; otherwise false. ПРОВЕРИТЬ
  * See: https://en.wikipedia.org/wiki/Primality_test
  *
  * @param {number} n
@@ -196,12 +198,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
- * Tries to convert value to number and returns it if conversion was successful;
+ * Tries to convert value to number and returns it if conversion was successful; СДЕЛАТЬ
  * otherwise returns default value passed as a second argument.
  *
  * @param {any} value
@@ -230,8 +237,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
